2a69b74a167e3a4447d1d8bc352479d652c99f56
Sasha Voynow <sasha@labzero.com>
2016-09-13 10:28:35 -0700

    simple counter

--- Main.elm
+++ Main.elm
@@ -1,58 +1,34 @@
-module Main exposing (..)
+module Main2 exposing (main)
 
+import Html.App as App
 import Html exposing (..)
+import Html.Events exposing (..)
 
-aString : String
-aString = "I am a String"
-
-aNumber : Int
-aNumber = 12345
-
-aList : List Int
-aList = [1, 2, 3, 4, 5]
-
-anotherList : List String
-anotherList = ["foo", "bar", "baz"]
-
-aTuple : (Int, String)
-aTuple = (12, "foo")
-
-myFunc : Int -> Int
-myFunc = \x -> x + 1
-
-sameFunc : Int -> Int
-sameFunc x = x + 1
-
-pointFreeFunc : Int -> Int
-pointFreeFunc = (+) 1
-
-type alias Person = 
+type alias Model = 
   {
-    name : String
-  , address : String
+    counter : Int
   }
 
--- myRecord : { name: String, address: String} -- boo!
-myRecord : Person  
-myRecord = 
+model : Model
+model = 
   {
-    name = "John Doe"
-  , address = "123 Main St."
+    counter = 0
   }
 
-item : String -> Html a
-item x = 
-  li [] [text x]
+type Msg 
+  = Increment
 
-main : Html a
-main =
-  ul []
-    [
-      item aString
-    , item (toString aNumber)
-    , item (toString aList)
-    , item (toString anotherList)
-    , item (toString aTuple)
-    , item (toString (myFunc aNumber))
-    , item (toString myRecord)    
-    ]
+view : Model -> Html Msg
+view model = div [] 
+  [
+    div [] [text (toString model.counter)]
+  , button [onClick Increment] [text "Click Me"]
+  ]
+
+update : Msg -> Model -> Model
+update msg model = 
+  case msg of
+    Increment -> {model | counter = model.counter + 1}
+
+main = 
+  App.beginnerProgram { model = model, view = view, update = update}
