982a9fc77eaf2318818e313fbbc6c97430ec42b7
Sasha Voynow <sasha@labzero.com>
2016-09-13 10:30:42 -0700

    decrement

--- Main.elm
+++ Main.elm
@@ -17,18 +17,21 @@ model =
 
 type Msg 
   = Increment
+  | Decrement
 
 view : Model -> Html Msg
 view model = div [] 
   [
     div [] [text (toString model.counter)]
-  , button [onClick Increment] [text "Click Me"]
+  , button [onClick Increment] [text "+"]
+  , button [onClick Decrement] [text "-"]
   ]
 
 update : Msg -> Model -> Model
 update msg model = 
   case msg of
     Increment -> {model | counter = model.counter + 1}
-
+    Decrement -> {model | counter = model.counter - 1}
+    
 main = 
   App.beginnerProgram { model = model, view = view, update = update}
