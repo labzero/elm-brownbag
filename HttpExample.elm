fdac38b8c070d9c1bc7adb5bcdd376e77ef764ec
Sasha Voynow <sasha@labzero.com>
2016-09-14 13:14:10 -0700

    new post

--- HttpExample.elm
+++ HttpExample.elm
@@ -6,8 +6,11 @@ module HttpExample exposing (..)
 import Html.App as App
 import Html exposing (..)
 import Http
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 import Task exposing (Task)
 import Json.Decode exposing (Decoder, (:=), object3, string, int)
+import Json.Encode exposing (encode)
 
 type alias Post = 
   { id : Int
@@ -24,10 +27,11 @@ type alias Model =
 
 type Msg 
   = LoadPosts (List Post)
-  | LoadPostsFailure Http.Error
+  | ApiFailure Http.Error
   | UpdateNewPostTitle String
   | UpdateNewPostBody String
   | SubmitNewPost
+  | NewPost Post
 
 
 initialModel : Model
@@ -39,8 +43,9 @@ initialModel =
 
 view : Model -> Html Msg
 view model = 
-  div [] 
-    (List.map postView model.posts) 
+  body [style [("margin", "50px")]] <| 
+    (List.map postView model.posts) ++
+    [postForm model] 
 
 postView : Post -> Html Msg
 postView post =
@@ -50,6 +55,29 @@ postView post =
   , p [] [text post.body] 
   ]
 
+postForm : Model -> Html Msg
+postForm model = 
+  div [] 
+  [
+    label [for "postTitle"] [text "Title"]
+  , input 
+    [
+      id "postTitle"
+    , value model.newPostTitle
+    , style [("display", "block")]
+    , onInput UpdateNewPostTitle
+    ] []
+  , label [for "postBody"] [text "Body"]
+  , textarea 
+    [
+      id "postBody"
+    , value model.newPostBody
+    , onInput UpdateNewPostBody
+    , style [("display", "block")]
+    ] []
+  , button [onClick SubmitNewPost] [text "Create New Post"]
+  ]
+
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
@@ -57,28 +85,55 @@ update msg model =
     LoadPosts posts -> 
       {model | posts = posts} ! []
 
-    LoadPostsFailure err ->
+    ApiFailure err ->
       model ! []
 
     UpdateNewPostTitle str ->
-      model ! []
+      { model | newPostTitle = str } ! []
     
     UpdateNewPostBody str ->
-      model ! []
+      { model | newPostBody = str } ! []
 
     SubmitNewPost ->
-      model ! []
+      model ! [submitPost model]
+
+    NewPost post ->
+      { model | posts = model.posts ++ [post], newPostTitle = "", newPostBody = "" } ! []
 
 -- Tasks
 getPostsTask : Task Http.Error (List Post)
 getPostsTask =
     Http.get postListDecoder "http://localhost:3000/posts"
 
+submitPostTask : Model -> Task Http.Error Post
+submitPostTask model = 
+    let
+        encoder = Json.Encode.object
+          [
+            ("userId", Json.Encode.int 1)
+          , ("title", Json.Encode.string model.newPostTitle)
+          , ("body", Json.Encode.string model.newPostBody) 
+          ]
+        body = encode 0 encoder |> Http.string
+        config =
+            { verb = "POST"
+            , headers = [ ( "Content-Type", "application/json" ) ]
+            , body = body
+            , url = "http://localhost:3000/posts"
+            }
+    in
+        Http.send Http.defaultSettings config |> Http.fromJson postDecoder      
+
 
 -- Cmds
 getPosts : Cmd Msg
 getPosts = 
-  getPostsTask |> Task.perform LoadPostsFailure LoadPosts  
+  getPostsTask |> Task.perform ApiFailure LoadPosts
+
+submitPost : Model -> Cmd Msg
+submitPost model = 
+  submitPostTask model |> Task.perform ApiFailure NewPost
+ 
 
 -- Decoders
 postDecoder : Decoder Post
@@ -91,6 +146,7 @@ postDecoder =
 postListDecoder : Decoder (List Post)
 postListDecoder =
   Json.Decode.list postDecoder
+
 
 main = App.program
   {
